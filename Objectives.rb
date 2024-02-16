fd8f0ec7a09ea4fd2f3a1465da4343714f1e8f09
Anthony Payton <paytonanthony2020@gmail.com>
2024-02-16 16:04:02 -0700

    inital files

---
+++ Objectives.rb
@@ -0,0 +1,23 @@
+=begin
+Basic Objectives
+Display a menu in the console for the user to interact with.
+Create a default array of hashes that represent items at a grocery store.
+Create a menu option to add items to a user's grocery cart.
+Create a menu option to display all the items in the cart.
+Create a menu option to remove an item from the users cart.
+Create a menu option to show the total cost of all the items in the user's cart.
+Add new items to the grocery store.
+Zip it up and turn it in!
+ 
+
+Bonus Objectives:
+Allow a user to remove multiple items at once from the cart.
+Assign the user an amount of money to start.
+Give the user the option to "Check out".
+If they have enough money, their cart gets cleared and money is subtracted.
+If they don't have enough money, they have to delete items.
+Apply some sort of coupon system.
+For example, a 20% off coupon that takes the price of all items down by 20%.
+Apply taxes to transaction.
+A menu option that shows a history of all the items purchased. (While the script runs)
+Grocery store items have a quantity. (They can be out of stock)
